6f86cda9f8ea907423e327fdc3fca03c297123e6
fre-ben <fschocke@gmail.com>
2021-05-04 20:01:16 +0200

    🎉 Initialize strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7b21bac1ad75752f361f1272f751081b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e59410a6-bea9-4f4e-b420-d2419b077b8c'
+};
